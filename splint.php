12114102a24a41add03b67134556ed4c2855374a
Francis Ilechukwu <francis94c@gmail.com>
2019-02-22 17:07:18 +0100

    Started the production server package and dependency manager.

---
+++ splint.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Splint Production Server Package and Dependency Manager.
+ * (c) CynoBit 2019
+ * Date Created: 22/2/2019 11:06 AM
+ * Author: Francis Ilechukwu
+ * Email:  francis.ilechukwu@cynobit.com
+ */
+// A 'splint.json' file is required to execute this script further.
+if (!is_file("splint.json")) die("Could not find a 'splint.json' file.");
+?>
